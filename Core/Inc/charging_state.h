96563f0795b36c93536394fe2d80496d5b63c0fc
RidhiGupta001 <142829845+RidhiGupta001@users.noreply.github.com>
2025-06-07 17:58:52 +0530

    implemented charging state and some balancing and checks functions in the normal state

---
+++ Core/Inc/charging_state.h
@@ -0,0 +1,28 @@
+#ifndef CHARGING_STATE_H
+#define CHARGING_STATE_H
+
+typedef enum{
+	voltage;
+	current;
+	temperature;
+}charging_parameters;
+
+
+typedef enum{
+	range_voltage,
+	range_current,
+	range_temperature,
+	maximum_chargeable_voltage;
+
+}charging_sof_Error_Checks;
+
+void charging_parameter_read(charging_parameters);
+void charging_cell_balancing(charging_parameters,charging_sof_Error_Checks);
+//this function must also be called if any cell voltage reaches its maximum voltage
+void charging_soc_Calculate(charging_parameters);
+void charging_sof_Calculate(charging_sof_Error_Checks);
+void charging_sop_Calculate(charging_parameters);
+void charging_soh_Calculate(charging_parameters);
+void charging_fan_speed_Control(int temperature); //for temperature control
+void charge_complete(charging_parameters,charging_sof_Error_Checks);
+// if all cells reached their maximum chargeable voltage end the charging
--- Core/Inc/normal_state.h
+++ Core/Inc/normal_state.h
@@ -11,7 +11,7 @@ typedef enum{
 	range_voltage,
 	range_current,
 	range_temperature,
-
+    minimum_cell_voltage
 }sof_Error_Checks;
 
 void parameter_read(input_parameters);
@@ -20,4 +20,7 @@ void sof_Calculate(sof_Error_Checks);
 void sop_Calculate(input_parameters);
 void soh_Calculate(input_parameters);
 void fan_speed_Control(int temperature);
-//fault handler incldue from main for each error
+void cell_balancing(input_parameters);
+void discharge_check(input_parameters,sof_Error_Checks);
+//this function checks if any cell is discharged below the minimum threshold voltage
+//fault handler include from main for each error
