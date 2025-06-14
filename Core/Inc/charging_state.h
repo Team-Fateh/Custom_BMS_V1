782bd87e9c751a30820693c2cda4004d656ea9f3
Tanishq Sahu <tanishqsahu101@gmail.com>
2025-06-14 16:19:01 +0530

    Update charging_state.h

--- Core/Inc/charging_state.h
+++ Core/Inc/charging_state.h
@@ -1,11 +1,11 @@
 #ifndef CHARGING_STATE_H
 #define CHARGING_STATE_H
 
-typedef enum{
-	voltage,
-	current,
-	temperature
-}charging_parameters;
+#define NUM_CELLS 112
+
+float voltage[NUM_CELLS];
+float current[NUM_CELLS];
+float temperature[NUM_CELLS];
 
 
 typedef enum{
@@ -16,13 +16,13 @@ typedef enum{
 
 }charging_sof_Error_Checks;
 
-void charging_parameter_read(charging_parameters);
-void charging_cell_balancing(charging_parameters,charging_sof_Error_Checks);
+void charging_parameter_read(float voltage,float current, float temperature);
+void charging_cell_balancing(float voltage,float current, float temperature,charging_sof_Error_Checks);
 //this function must also be called if any cell voltage reaches its maximum voltage
-void charging_soc_Calculate(charging_parameters);
+void charging_soc_Calculate(float voltage,float current, float temperatur);
 void charging_sof_Calculate(charging_sof_Error_Checks);
-void charging_sop_Calculate(charging_parameters);
-void charging_soh_Calculate(charging_parameters);
+void charging_sop_Calculate(float voltage,float current, float temperature);
+void charging_soh_Calculate(float voltage,float current, float temperature);
 void charging_fan_speed_Control(int temperature); //for temperature control
-void charge_complete(charging_parameters,charging_sof_Error_Checks);
+void charge_complete(float voltage,float current, float temperature,charging_sof_Error_Checks);
 // if all cells reached their maximum chargeable voltage end the charging
