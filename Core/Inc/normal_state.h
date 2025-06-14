70fbc0480a10703948374585ddf2d756143bec75
Tanishq Sahu <tanishqsahu101@gmail.com>
2025-06-14 17:52:12 +0530

    Update normal_state.h

--- Core/Inc/normal_state.h
+++ Core/Inc/normal_state.h
@@ -14,13 +14,13 @@ typedef enum{
     minimum_cell_voltage
 }sof_Error_Checks;
 
-void parameter_read(float voltage,float current, float temperature);
-void soc_Calculate(float voltage,float current, float temperature);
+void parameter_read(float *voltage,float *current, float *temperature);
+void soc_Calculate(float *voltage,float *current, float *temperature);
 void sof_Calculate(sof_Error_Checks);
-void sop_Calculate(float voltage,float current, float temperature);
-void soh_Calculate(float voltage,float current, float temperature);
+void sop_Calculate(float *voltage,float *current, float *temperature);
+void soh_Calculate(float *voltage,float *current, float *temperature);
 void fan_speed_Control(int temperature);
-void cell_balancing(float voltage,float current, float temperature);
-void discharge_check(float voltage,float current, float temperature,sof_Error_Checks);
+void cell_balancing(float *voltage,float *current, float *temperature);
+void discharge_check(float *voltage,float *current, float *temperature,sof_Error_Checks);
 //this function checks if any cell is discharged below the minimum threshold voltage
 //fault handler include from main for each error
